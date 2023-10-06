de182f69a90867e47a1e9bb4574b2f700279ef5b
rrudiansyah <107456020+rrudiansyah@users.noreply.github.com>
2023-10-06 14:50:35 +0700

    Add files via upload

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+st.header('Bike Sharing Dashboard :sparkles:')
+
+datetime_columns = ["Tgl Permohonan Lab", "Tgl Hasil dilaporkan", "tanggal"]
+
+all_df = pd.read_csv("dashboard/all_data.csv")
+
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
